eca77adf374ece759e09b9fb0986ade2e7b59860
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2020-11-25 00:58:54 -0600

    added inquirer prompt questions for users to answer

--- index.js
+++ index.js
@@ -2,10 +2,60 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// array of questions for user
-const questions = [
-
-];
+//questions for user to answer
+inquirer.prompt([
+    { 
+       message: "What is your GitHub username?", 
+       name: "userName",
+       type: "input"
+    },
+    {
+        message: "What is your email address?",
+        name: "email",
+        type: "input"
+    },
+    {
+        message: "What is your project's name?",
+        name: "project",
+        type: "input"
+    },
+    {
+        message: "Please write a short description of your project",
+        name: "projectInput",
+        type: "input"
+    },
+    {
+        message: "What kind of license should your project have?",
+        name: "license",
+        type: "list",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "None"]
+    },
+    {
+        message: "What command should be run to install dependencies?",
+        name: "npmInput",
+        type: "default",
+        default: "npm i"
+    },
+    {
+        message: "What command should be run to run tests?",
+        name: "npmTest",
+        type: "default",
+        default: "npm test"
+    },
+    {
+        message: "What does the user need to know about using the repo?",
+        name: "useRepo",
+        type: "input"
+    },
+    {
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributeRepo",
+        type: "input"
+    }
+])
+.then(function(answers) {
+console.log(answers);
+})
 
 // function to write README file
 function writeToFile(fileName, data) {
