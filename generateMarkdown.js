30a259c378b60de2f04e50d61d44f4ffdcb569b6
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2020-11-25 18:04:37 -0600

    saving changes I made to structure the readme.md file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,47 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+#Project Name
+${data.project}
+
+# Description
+${data.projectInput}
+
+#Table of Contents
+*[Project name](#Project name)
+*[Project Description](#Description)
+*[License](#License)
+*[Installation](#Installation)
+*[Run tests](#Run Tests)
+*[Using Repo](#Using Repo)
+*[Contribute to repo](#Contribute to repo)
+*[Questions](#Questions)
+
+
+# License
+${data.license}
+
+# Installation
+${data.npmInput}
+
+#Run tests
+${data.npmTest}
+
+#Using Repo
+${data.useRepo}
+
+#Contributing to repo
+${data.contributeRepo}
+
+
+$Demo/Screenshot
+
+#Quesitons
+If you have any questions please use the contacts down below:
+
+#### Email address: ${data.email}
+#### GitHub account: ${data.userName}
+
 `;
 }
 
